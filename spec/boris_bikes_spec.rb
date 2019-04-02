b74420abdd2e511f32daa08d47e4ce693d26a434
MaxRobertsDear <maxhroberts@gmail.com>
2019-04-02 21:19:43 +0100

    make DockingStation rspec one-liner

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,8 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'responds to releases bike' do
-    expect(subject).to respond_to : release_bike
+  it { is_expected.to respond_to :release_bike }
   end
 end
 
